54b3c7d8ba22e18de76f6e7b09fe323851cf8c03
Tanja <tanja120a@gmail.com>
2021-12-26 18:30:15 +0100

    Create server with routes

--- README.md
+++ README.md
@@ -34,10 +34,10 @@
 Inside every folder we are going to have some or all of modules
 
  - Route configuration - to define the requests our API can handle
- - Services - for tasks such as connecting to our database models, doing queries, or connecting to external services that are required by the specific request
+ - Services - for tasks such as connecting to our database models, doing queries, or connecting to external services that are required by the specific request, make our code cleaner by encapsulating business logic operations into functions that middleware and controllers can call
  - Middleware - for running specific request validations before the final controller of a route handles its specifics
  - Models - for defining data models matching a given database schema, to facilitate data storage and retrieval
- - Controllers - for separating the route configuration from the code that finally (after any middleware) processes a route request, calls the above service functions if necessary, and gives a response to the 
+ - Controllers - for separating the route configuration from the code that finally (after any middleware) processes a route request, calls the above service functions if necessary, and gives a response to the client.
 
 ## Build with 
     1. Node.js
---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import http from 'http';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import { CommonRoutesConfig } from './common/common.routes.config';
+import cors from 'cors';
+import debug from 'debug';
+import { UsersRoutes } from './users/users.routes.config';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+
+//Adding middleware to parse all incoming request as JSON
+app.use(express.json());
+
+//Adding Middleware for cross-origin requests
+app.use(cors());
+
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({all: true}),
+    ),
+};
+
+if(!process.env.DEBUG) {
+    loggerOptions.meta = false;
+}
+
+//Initialize the logger
+app.use(expressWinston.logger(loggerOptions));
+
+//Adding User Routes to array of routes
+routes.push( new UsersRoutes(app));
+
+const PORT = process.env.PORT || 3000;
+const runningMessages = `Listening port ${PORT}`;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(runningMessages);
+})
+
+//Start our server
+server.listen(PORT, () => {
+    routes.forEach(route =>{
+        debugLog(`Routes configure for route ${route.getName()}`);
+    })
+    console.log(runningMessages);
+});
